c41f59904d93d8831a3993195b9f95806d4e39c9
Iznsraa03 <waisalqarni1712@gmail.com>
2026-08-10 02:07:25 +0800

    Fix User model mass assignment property

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,12 +12,17 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
+
+    protected $guarded = [];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 
     /**
      * Get the attributes that should be cast.
